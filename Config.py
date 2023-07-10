a93d8e6755518bb882a461d3fba8ff30f1245068
yuvraj371 <139117085+yuvraj371@users.noreply.github.com>
2023-07-10 22:24:14 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -6,22 +6,22 @@ id_pattern = re.compile(r'^.\d+$')
 
 # Bot information
 SESSION = environ.get('SESSION', 'LuciferMoringstar_Robot')
-API_ID = int(environ['API_ID'])
-API_HASH = environ['API_HASH']
-BOT_TOKEN = environ['BOT_TOKEN']
+API_ID = int(environ['23104044'])
+API_HASH = environ['f02a56885f32a83417b2b266d18473a8']
+BOT_TOKEN = environ['6388550170:AAHIXJZAN2tgAbhDYrFIE5ectAgNdu7YZIY']
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
 
-BROADCAST_CHANNEL = int(os.environ.get("BROADCAST_CHANNEL", ""))
-ADMIN_ID = set(int(x) for x in os.environ.get("ADMIN_ID", "").split())
-DB_URL = os.environ.get("DATABASE_1", "")
+BROADCAST_CHANNEL = int(os.environ.get("https://t.me/+yyijzRAlsCszZDdl", ""))
+ADMIN_ID = set(int(x) for x in os.environ.get("5980613692", "").split())
+DB_URL = os.environ.get("mongodb+srv://fsclecture:yuvraj178@cluster0.rt7bwpq.mongodb.net/?retryWrites=true&w=majority", "")
 BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST", True))
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ['ADMINS'].split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ['CHANNELS'].split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ['5980613692'].split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ['-1001660689741'].split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('FORCES_SUB')
@@ -29,8 +29,8 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(admin) for admin in environ.get("AUTH_GROUPS", "").split()]
 TUTORIAL = "https://youtu.be/5hnYOKBzyi8"
 # MongoDB information
-DATABASE_URI = environ['DATABASE_2']
-DATABASE_NAME = environ['BOT_NAME']
+DATABASE_URI = environ['mongodb+srv://fsclecture:yuvraj178@cluster0.rt7bwpq.mongodb.net/?retryWrites=true&w=majority']
+DATABASE_NAME = environ['fsclecture']
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Messages
@@ -44,7 +44,9 @@ START_MSG = environ.get('START_MSG', default_start_msg)
 FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "")
 OMDB_API_KEY = environ.get("OMDB_API_KEY", "http://www.omdbapi.com/?i=tt3896198&apikey=4f08a979")
 if FILE_CAPTION.strip() == "":
-    CUSTOM_FILE_CAPTION=None
+    CUSTOM_FILE_CAPTION= please Join our main channel
+
+Here is link 🔗 https://t.me/+yyijzRAlsCszZDdl
 else:
     CUSTOM_FILE_CAPTION=FILE_CAPTION
 if OMDB_API_KEY.strip() == "":
